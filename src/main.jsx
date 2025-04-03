c6852f37b451bc1d0b66e8057047d669b1357d98
Genta Bahana Nagari <gentapossenti@gmail.com>
2025-04-03 16:55:53 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Porto from "./components/Porto";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <div className="fixed -z-10 min-h-screen w-full bg-black"></div>
-
-    <main className="flex flex-col items-center px-4 md:px-8 lg:px-16 text-white">
-      <Navbar />
-      <Hero className="py-1" />
-      <About className="py-1" />
-      <Skills className="py-1" />
-      <Porto className="py-1" />
-      <Contact className="py-1" />
-      <Footer className="py-1" />
-    </main>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
